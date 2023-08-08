f07a8529c0b334d13699511d6e58fcefc9e245bc
Ayush-Dabas <aydbs23@gmail.com>
2023-08-08 08:16:49 +0530

    added the javascript working without any issue

--- stopwatch/app.js
+++ stopwatch/app.js
@@ -1,5 +1,46 @@
-function start() {}
+window.onload = function () {
+  var currentSec = document.getElementById("sec");
+  var currentMilSec = document.getElementById("milsec");
+  var sec = 0o0;
+  var tens = 0o0;
+  var buttonStart = document.getElementById("start");
+  var buttonStop = document.getElementById("stop");
+  var buttonReset = document.getElementById("reset");
+  var Interval;
 
-function stop() {}
+  buttonStart.onclick = function () {
+    clearInterval(Interval);
+    Interval = setInterval(startTimer, 10);
+  };
+  buttonStop.onclick = function () {
+    clearInterval(Interval);
+  };
+  buttonReset.onclick = function () {
+    clearInterval(Interval);
+    tens = "00";
+    sec = "00";
+    currentMilSec.innerHTML = tens;
+    currentSec.innerHTML = sec;
+  };
+  function startTimer() {
+    tens++;
 
-function reset() {}
+    if (tens <= 9) {
+      currentMilSec.innerHTML = "0" + tens;
+    }
+    if (tens > 9) {
+      currentMilSec.innerHTML = tens;
+    }
+
+    if (tens > 99) {
+      console.log("seconds");
+      sec++;
+      currentSec.innerHTML = "0" + sec;
+      tens = 0;
+      currentMilSec.innerHTML = "0" + 0;
+    }
+    if (seconds > 9) {
+      currentSec.innerHTML = sec;
+    }
+  }
+};
