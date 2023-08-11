c00b3648e4f44adaa76377ced5d4e275951aceb4
Ayush-Dabas <aydbs23@gmail.com>
2023-08-11 17:10:24 +0530

    added some changes

--- canvas game/app.js
+++ canvas game/app.js
@@ -23,10 +23,23 @@ function move() {
   //   const cS = dX * dY === 0 ? 1 : 0.707;
   //   player.posX += cS * dX;
   //   player.posY += cS * dY;
+  if ((dX > 300 || dX < -300) && (dY < -200 || dY > 190)) {
+    dX = 0;
+    dY = 0;
+  }
   player.posX += dX;
   player.posY += dY;
+  // checkBoundry();
+
   player.el.style.transform = `translate(${player.posX}px, ${player.posY}px)`;
   requestAnimationFrame(move);
+}
+
+function checkBoundry() {
+  setInterval(() => {
+    console.log(player.posX);
+    console.log(player.posY);
+  }, 2000);
 }
 
 document.addEventListener("keyup", keyStrokes);
